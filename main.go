dc8bd6ca521243b5dad2b6008e8684171e465725
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-12 18:27:48 +0900

    json

--- main.go
+++ main.go
@@ -1,108 +1,28 @@
-// The server program issues Google search requests and demonstrates the use of
-// the go.net Context API. It serves on port 8080.
-//
-// The /search endpoint accepts these query params:
-//   q=the Google search query
-//   timeout=a timeout for the request, in time.Duration format
-//
-// For example, http://localhost:8080/search?q=golang&timeout=1s serves the
-// first few Google search results for "golang" or a "deadline exceeded" error
-// if the timeout expires.
 package main
 
 import (
-	"context"
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"log"
-	"net/http"
-	"text/template"
-	"time"
-
-	"github.com/richardimaoka/go-sample/google"
-	"github.com/richardimaoka/go-sample/userip"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/search", handleSearch)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// handleSearch handles URLs like /search?q=golang&timeout=1s by forwarding the
-// query to google.Search. If the query param includes timeout, the search is
-// canceled after that duration elapses.
-func handleSearch(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("handleSearch")
-	// ctx is the Context for this handler. Calling cancel closes thec
-	// ctx.Done channel, which is the cancellation signal for requests
-	// started by this handler.
-	var (
-		ctx    context.Context
-		cancel context.CancelFunc
-	)
-	timeout, err := time.ParseDuration(req.FormValue("timeout"))
-	fmt.Println(req.FormValue("timeout"))
-	if err == nil {
-		// The request has a timeout, so create a context that is
-		// canceled automatically when the timeout expires.
-		fmt.Printf("the request has timeout = %v\n", timeout)
-		ctx, cancel = context.WithTimeout(context.Background(), timeout)
-	} else {
-		fmt.Printf("failed to get the request timeout with error = %v\n", err)
-		ctx, cancel = context.WithCancel(context.Background())
+	type Road struct {
+		Name   string
+		Number int
 	}
-	defer cancel() // Cancel ctx as soon as handleSearch returns.
-
-	query := req.FormValue("q")
-	if query == "" {
-		http.Error(w, "no query", http.StatusBadRequest)
-		return
+	roads := []Road{
+		{"Diamond Fork", 29},
+		{"Sheep Creek", 51},
 	}
 
-	// Store the user IP in ctx for use by code in other packages.
-	userIP, err := userip.FromRequest(req)
+	b, err := json.Marshal(roads)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	ctx = userip.NewContext(ctx, userIP)
-	fmt.Printf("userIP = %v\n", userIP)
-
-	// Run the Google search and print the results.
-	start := time.Now()
-	fmt.Println("starting google search")
-	results, err := google.Search(ctx, query)
-	fmt.Println(results)
-	elapsed := time.Since(start)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := resultsTemplate.Execute(w, struct {
-		Results          google.Results
-		Timeout, Elapsed time.Duration
-	}{
-		Results: results,
-		Timeout: timeout,
-		Elapsed: elapsed,
-	}); err != nil {
-		log.Print(err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println(userIP)
-	fmt.Println(ctx)
+	var out bytes.Buffer
+	json.Indent(&out, b, "=", "\t")
+	out.WriteTo(os.Stdout)
 }
-
-var resultsTemplate = template.Must(template.New("results").Parse(`
-<html>
-<head/>
-<body>
-  <ol>
-  {{range .Results}}
-    <li>{{.Title}} - <a href="{{.URL}}">{{.URL}}</a></li>
-  {{end}}
-  </ol>
-  <p>{{len .Results}} results in {{.Elapsed}}; timeout {{.Timeout}}</p>
-</body>
-</html>
-`))
