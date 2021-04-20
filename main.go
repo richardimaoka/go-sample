a00bc8815e2ffe1a123f563f5df0cd32533b19ce
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-20 22:50:20 +0900

    intB

--- main.go
+++ main.go
@@ -23,15 +23,15 @@ func main() {
 	intB, _ := json.Marshal(1)
 	fmt.Println(string(intB))
 
-	// fltB, _ := json.Marshal(2.34)
-	// fmt.Println(string(fltB))
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
 
-	// strB, _ := json.Marshal("gopher")
-	// fmt.Println(string(strB))
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
 
-	// slcD := []string{"apple", "peach", "pear"}
-	// slcB, _ := json.Marshal(slcD)
-	// fmt.Println(string(slcB))
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
 
 	// mapD := map[string]int{"apple": 5, "lettuce": 7}
 	// mapB, _ := json.Marshal(mapD)
