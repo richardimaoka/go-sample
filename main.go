1030dc9e1220bbb9ddc15fcb1d7341badbbff12b
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-03 03:34:34 +0900

    mock mock

--- main.go
+++ main.go
@@ -11,18 +11,33 @@ type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+type MockDoType func(req *http.Request) (*http.Response, error)
+
+type MockClient struct {
+	MockDo MockDoType
+}
+
+func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
+	return m.MockDo(req)
+}
+
 var (
 	Client HTTPClient
 )
 
 func main() {
-	Client = &http.Client{}
+	Client = &MockClient{
+		MockDo: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+			}, nil
+		},
+	}
 
 	request, err := http.NewRequest(http.MethodGet, "https://google.com", nil)
 	if err != nil {
 		log.Fatal("Request invalid")
 	}
-
 	response, err := Client.Do(request)
 	if err != nil {
 		log.Fatalf("response invalid")
