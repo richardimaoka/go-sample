b022a187cc3ad991d01fd134631682cf8f6129be
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-09-17 02:50:30 +0900

    success

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"reflect"
+)
 
 func TestSum(t *testing.T) {
 
@@ -33,7 +37,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
