e14296d53839ca28f059e3c8be3ff94045e82795
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-04 23:32:08 +0900

    remove errors

--- mocks/main.go
+++ mocks/main.go
@@ -11,11 +11,6 @@ func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
 }
 
-func CountdownB(out bytes.Buffer) {
-	fmt.Fprint(out, "3")
-}
-
 func main() {
 	Countdown(os.Stdout)
-	CountdownB(os.Stdout)
 }
