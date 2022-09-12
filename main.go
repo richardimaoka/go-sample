09e32f6d6ea49511e0e422e8cd2b83c270eb5e59
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-13 08:59:05 +0900

    file

--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 )
 
 func main() {
+	fmt.Println("place a")
 	file, err := os.Open("file.go") // For read access.
 	if err != nil {
+		fmt.Println("error place 1")
 		log.Fatal(err)
 	}
+	fmt.Println("place B")
 	data := make([]byte, 100)
 	count, err := file.Read(data)
+	fmt.Println(" place C")
 	if err != nil {
+		fmt.Println("error place 2")
 		log.Fatal(err)
 	}
 	fmt.Printf("read %d bytes: %q\n", count, data[:count])
