221f68593f11f1e424fdb2d70a856fdc102f17d8
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-13 09:14:02 +0900

    wrong stdin usage

--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("place a")
-	file, err := os.Open("file.go") // For read access.
-	if err != nil {
-		fmt.Println("error place 1")
-		log.Fatal(err)
-	}
-	fmt.Println("place B")
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	fmt.Println(" place C")
-	if err != nil {
-		fmt.Println("error place 2")
-		log.Fatal(err)
-	}
-	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+	buf := []byte{}
+	n, _ := os.Stdin.Read(buf)
+	fmt.Println("Read", n, "lines")
+	fmt.Println("Buf", string(buf))
 }
