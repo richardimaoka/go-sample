957d55878b3ee8b0be1b7451ba894014af2906e8
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-24 05:06:59 +0900

    ticked countdown

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Commencing countdown")
-	// tick := time.Tick(1 * time.Second)
+	tick := time.Tick(1 * time.Second)
 	for countdown := 10; countdown > 0; countdown-- {
 		fmt.Println(countdown)
+		<-tick
 	}
 }
