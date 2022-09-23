2c672a490ddf84da97615cba410b56c2c9be60bd
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-24 05:38:42 +0900

    launch and numbers

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func launch() {
 	// UTF-8  Encoding: 0xF0 0x9F 0x9A 0x80
 	// UTF-16 Encoding: 0xD83D 0xDE80
 	// UTF-32 Encoding: 0x0001F680
-	fmt.Println("\xF0\x9F\x9A\x80")
+	fmt.Print("\xF0\x9F\x9A\x80")
 	// fmt.Println("\uD83D\uDE80") // doesn't compile
 	// fmt.Println("\u1F680") //wrong!
-	fmt.Println("\U0001F680")
+	fmt.Println("\U0001F680 launched!!!")
 }
 
 func main() {
@@ -30,10 +30,20 @@ func main() {
 		fmt.Println("aborted!!")
 		return
 	}
+	launch()
+
 	// tick := time.Tick(100 * time.Millisecond)
 	// for countdown := 10; countdown > 0; countdown-- {
 	// 	fmt.Println(countdown)
 	// 	<-tick
 	// }
-	launch()
+
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x) //"0" "2" "4" "6" "8"
+		case ch <- i:
+		}
+	}
 }
