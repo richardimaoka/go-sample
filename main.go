29d7d3916dea3d92d9338377e5b4ed75358ee130
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-24 05:06:25 +0900

    countdown

--- main.go
+++ main.go
@@ -1,38 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"os"
+	"fmt"
 )
 
-type Message struct {
-	Name string
-	Body string
-	Time int64
-}
-
 func main() {
-	type Genre struct {
-		Country string
-		Rock    string
+	fmt.Println("Commencing countdown")
+	// tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
 	}
-
-	type Music struct {
-		Genre Genre
-	}
-
-	resp := Music{
-		Genre: Genre{ // error on this line.
-			Country: "Taylor Swift",
-			Rock:    "Aimee",
-		},
-	}
-
-	js, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("cannot marshal")
-	}
-	os.Stdout.Write(js)
-
 }
