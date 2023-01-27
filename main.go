5a9a0354f57319c038501948012559c20738e011
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-27 23:36:43 +0900

    ok

--- main.go
+++ main.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
-	"time"
 )
 
-func launch() {
-	// UTF-8  Encoding: 0xF0 0x9F 0x9A 0x80
-	// UTF-16 Encoding: 0xD83D 0xDE80
-	// UTF-32 Encoding: 0x0001F680
-	fmt.Print("\xF0\x9F\x9A\x80")
-	// fmt.Println("\uD83D\uDE80") // doesn't compile
-	// fmt.Println("\u1F680") //wrong!
-	fmt.Println("\U0001F680 launched!!!")
+type TerminalElement interface {
+	IsTerminalElement()
+}
+
+type Terminal struct {
+	Name             *string           `json:"name"`
+	CurrentDirectory []*string         `json:"currentDirectory"`
+	Elements         []TerminalElement `json:"elements"`
+}
+
+type TerminalCommand struct {
+	Command *string `json:"command"`
+}
+
+func (TerminalCommand) IsTerminalElement() {}
+
+type TerminalOutput struct {
+	Output *string `json:"output"`
+}
+
+func (TerminalOutput) IsTerminalElement() {}
+
+type OkStruc struct {
+	Typename *string `json:"__typename"`
+	Output   *string
+	Command  *string
 }
 
 func main() {
-	abort := make(chan struct{})
-	go func() {
-		os.Stdin.Read(make([]byte, 1)) // read 1 byte
-		abort <- struct{}{}
-	}()
-
-	fmt.Println("Commencing countdown. Press return to abort.")
-	tick := time.NewTicker(100 * time.Millisecond)
-	for countdown := 10; countdown > 0; countdown-- {
-		fmt.Println(countdown)
-		select {
-		case <-tick.C:
-		case <-abort:
-			fmt.Println("aborted!!")
-			return
+	bytes := []byte(`[
+		{
+			"__typename": "TerminalCommand",
+			"index": 1,
+			"command": "protoc \\\n  --go_out=outdir --go_opt=paths=source_relative \\\n  helloworld.proto"
+		},
+		{
+			"__typename": "TerminalOutput",
+			"index": 2,
+			"output": "outdir/: No such file or directory"
+		},
+		{
+			"__typename": "TerminalCommand",
+			"command": "# protoc-go-experiments/helloworld/helloworld.proto\ncat << EOF > helloworld.proto\nsyntax = \"proto3\";\n\n// The greeting service definition.\nservice Greeter {\n  // Sends a greeting\n  rpc SayHello (HelloRequest) returns (HelloReply) {}\n}\n\n// The request message containing the user's name.\nmessage HelloRequest {\n  string name = 1;\n}\n\n// The response message containing the greetings\nmessage HelloReply {\n  string message = 1;\n}\nEOF"
 		}
+	]`)
+
+	var data []OkStruc
+	json.Unmarshal(bytes, &data)
+
+	for _, v := range data {
+		j, err := json.Marshal(v)
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%s\n", j)
 	}
-	tick.Stop()
-	launch()
+	fmt.Println("------------------")
 }
