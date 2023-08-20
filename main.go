ffba8fa71dd92678354260c993c52898343b3bc2
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-20 20:56:29 +0900

    image width height

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+)
 
+//https://gist.github.com/sergiotapia/7882944
+func main() {
+	width, height := getImageDimension("images/2mhQeVA6_400x400.jpg")
+	fmt.Println("Width:", width, "Height:", height)
+}
+
+func getImageDimension(imagePath string) (int, int) {
+	file, err := os.Open(imagePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	image, _, err := image.DecodeConfig(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
+	}
+	return image.Width, image.Height
 }
