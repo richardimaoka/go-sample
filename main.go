8e718d9166091dd9039b9d4519733380b2ebf1f2
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-06-29 06:16:21 +0900

    type Option

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-type (
-	Me struct {
-		V1 int
-		V2 string
-		V3 int
-		V4 string
-	}
-)
+type Me struct {
+	V1 int
+	V2 string
+	V3 int
+	V4 string
+}
 
-func New(v1 int, v2 string, opts ...func(me *Me)) *Me {
+type Option func(me *Me)
+
+func New(v1 int, v2 string, opts ...Option) *Me {
 	me := new(Me)
 
 	me.V1 = v1
@@ -26,13 +26,25 @@ func New(v1 int, v2 string, opts ...func(me *Me)) *Me {
 	return me
 }
 
+func WithV3(v int) Option {
+	return func(me *Me) {
+		me.V3 = v
+	}
+}
+
+func WithV4(v string) Option {
+	return func(me *Me) {
+		me.V4 = v
+	}
+}
+
 func main() {
 	m1 := New(1, "hello")
 	fmt.Printf("%v\n", m1)
 
-	m2 := New(2, "world", func(me *Me) { me.V4 = "golang" })
+	m2 := New(2, "world", WithV4("golang"))
 	fmt.Printf("%v\n", m2)
 
-	m3 := New(3, "naruhodo", func(me *Me) { me.V2 = "golang" })
+	m3 := New(3, "naruhodo", WithV3(10))
 	fmt.Printf("%v\n", m3)
 }
