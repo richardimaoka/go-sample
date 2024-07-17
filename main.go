dc8eb20eb12a3fb299cf9b7d9416cd0784ca1f8a
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-18 04:09:30 +0900

    set get value

--- main.go
+++ main.go
@@ -7,14 +7,44 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 )
 
+type TraceID string
+type AnotherID string
+
+const ZeroTraceID = ""
+
+type traceIDKey struct{}
+type anotherIDKey struct{}
+
+func SetTraceID(ctx context.Context, tid TraceID) context.Context {
+	return context.WithValue(ctx, traceIDKey{}, tid)
+}
+func GetTraceID(ctx context.Context) TraceID {
+	if v, ok := ctx.Value(traceIDKey{}).(TraceID); ok {
+		return v
+	}
+	return ZeroTraceID
+}
+
+func SetAnotherID(ctx context.Context, aid AnotherID) context.Context {
+	return context.WithValue(ctx, anotherIDKey{}, aid)
+}
+func GetAnotherID(ctx context.Context) AnotherID {
+	if v, ok := ctx.Value(anotherIDKey{}).(AnotherID); ok {
+		return v
+	}
+	return ZeroTraceID
+}
+
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
-	defer cancel()
-	go func() { fmt.Println("別ゴルーチン") }()
-	fmt.Println("STOP")
-	<-ctx.Done()
-	fmt.Println("そして時は動き出す")
+	ctx := context.Background()
+	fmt.Printf("trace id = %q\n", GetTraceID(ctx))
+	ctx = SetTraceID(ctx, "test-id")
+	fmt.Printf("trace id = %q\n", GetTraceID(ctx))
+
+	fmt.Printf("another id = %q\n", GetAnotherID(ctx))
+	ctx = SetAnotherID(ctx, "tessssst-id")
+	fmt.Printf("another id = %q\n", GetAnotherID(ctx))
+	fmt.Printf("trace id = %q\n", GetTraceID(ctx))
 }
