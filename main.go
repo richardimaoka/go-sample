dd1d0fb9e0d5b0b2301e66cc883d1940fafe67ff
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-18 03:55:42 +0900

    dio

--- main.go
+++ main.go
@@ -7,19 +7,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
-func child(ctx context.Context) {
-	// 関数の実ロジックに入る前にcontext.Contextの状態を検証する
-	if err := ctx.Err(); err != nil {
-		fmt.Println("キャンセルされている")
-		return
-	}
-	fmt.Println("キャンセルされていない")
-}
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	child(ctx)
-	cancel()
-	child(ctx)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
+	go func() { fmt.Println("別ゴルーチン") }()
+	fmt.Println("STOP")
+	<-ctx.Done()
+	fmt.Println("そして時は動き出す")
 }
