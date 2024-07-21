8659e2d1544aa3a27cd75f32f9f81db587b4f3ee
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-21 18:32:52 +0900

    repository.go

--- store/repository.go
+++ store/repository.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 
@@ -11,6 +12,44 @@ import (
 	"github.com/richardimaoka/go-sandbox/clock"
 	"github.com/richardimaoka/go-sandbox/config"
 )
+
+const (
+	// ErrCodeMySQLDuplicateEntry はMySQL系のDUPLICATEエラーコード
+	// https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html
+	// Error number: 1062; Symbol: ER_DUP_ENTRY; SQLSTATE: 23000
+	ErrCodeMySQLDuplicateEntry = 1062
+)
+
+var (
+	ErrAlreadyEntry = errors.New("duplicate entry")
+)
+
+func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
+	// sqlx.Connectを使うと内部でpingする。
+	db, err := sql.Open("mysql",
+		fmt.Sprintf(
+			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
+			cfg.DBUser, cfg.DBPassword,
+			cfg.DBHost, cfg.DBPort,
+			cfg.DBName,
+		),
+	)
+	if err != nil {
+		return nil, func() {}, err
+	}
+	// Openは実際に接続テストが行われない。
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, func() { _ = db.Close() }, err
+	}
+	xdb := sqlx.NewDb(db, "mysql")
+	return xdb, func() { _ = db.Close() }, nil
+}
+
+type Repository struct {
+	Clocker clock.Clocker
+}
 
 type Beginner interface {
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
@@ -24,6 +63,7 @@ type Execer interface {
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
 }
+
 type Queryer interface {
 	Preparer
 	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
@@ -33,40 +73,10 @@ type Queryer interface {
 }
 
 var (
-	// インターフェースが期待通りに宣言されているか確認
+	// インターフェイスが期待通りに宣言されているか確認
 	_ Beginner = (*sqlx.DB)(nil)
 	_ Preparer = (*sqlx.DB)(nil)
 	_ Queryer  = (*sqlx.DB)(nil)
 	_ Execer   = (*sqlx.DB)(nil)
 	_ Execer   = (*sqlx.Tx)(nil)
 )
-
-type Repository struct {
-	Clocker clock.Clocker
-}
-
-func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
-	// sqlx.Connectを使うと内部でpingする。
-	db, err := sql.Open("mysql",
-		fmt.Sprintf(
-			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
-			cfg.DBUser, cfg.DBPassword,
-			cfg.DBHost, cfg.DBPort,
-			cfg.DBName,
-		),
-	)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Openは実際に接続テストが行われない。
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
-	defer cancel()
-	if err := db.PingContext(ctx); err != nil {
-		return nil, func() { _ = db.Close() }, err
-	}
-
-	xdb := sqlx.NewDb(db, "mysql")
-
-	return xdb, func() { _ = db.Close() }, nil
-}
