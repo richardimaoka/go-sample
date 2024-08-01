396746779959de064fa3bc1cee82145a706a0876
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-02 02:46:12 +0900

    X509

--- main.go
+++ main.go
@@ -18,7 +18,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"net"
 	"net/http"
+	"os"
+	"time"
 )
 
 type MyHandler struct {
@@ -29,12 +39,41 @@ func (h *MyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	handler := MyHandler{}
-
-	server := http.Server{
-		Addr:    "localhost:8080",
-		Handler: &handler,
+	max := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, _ := rand.Int(rand.Reader, max)
+	subject := pkix.Name{
+		Organization:       []string{"Manning Publications Co."},
+		OrganizationalUnit: []string{"Books"},
+		CommonName:         "Go Web Programming",
 	}
 
-	server.ListenAndServeTLS("cert.pem", "key.pem")
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject:      subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+	}
+
+	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
+	certOut, _ := os.Create("cert.pem")
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+
+	keyOut, _ := os.Create("key.pem")
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+
+	fmt.Println("finished")
+	// handler := MyHandler{}
+
+	// server := http.Server{
+	// 	Addr:    "localhost:8080",
+	// 	Handler: &handler,
+	// }
+
+	// server.ListenAndServeTLS("cert.pem", "key.pem")
 }
