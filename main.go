43c731d01a9b5460603bac96cdf2c3fdc946d0f0
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-09 05:36:13 +0900

    map store

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
-// リスト5.2
+// リスト6.1
 package main
 
 import (
-	"html/template"
-	"net/http"
+	"fmt"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("tmpl.html")
-	t.Execute(w, "Hello World!")
+type Post struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var PostById map[int]*Post
+var PostsByAuthor map[string][]*Post
+
+func store(post Post) {
+	PostById[post.Id] = &post
+	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
 }
 
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
+
+	PostById = make(map[int]*Post)
+	PostsByAuthor = make(map[string][]*Post)
+
+	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
+	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
+	post3 := Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"}
+	post4 := Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"}
+
+	store(post1)
+	store(post2)
+	store(post3)
+	store(post4)
+
+	fmt.Println(PostById[1])
+	fmt.Println(PostById[2])
+
+	for _, post := range PostsByAuthor["Sau Sheong"] {
+		fmt.Println(post)
 	}
-	http.HandleFunc("/process", process)
-	server.ListenAndServe()
+	for _, post := range PostsByAuthor["Pedro"] {
+		fmt.Println(post)
+	}
 }
