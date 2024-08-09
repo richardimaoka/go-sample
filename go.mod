975b9c54f644025cae3a22bcfe5443018569fe7d
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-10 08:46:36 +0900

    in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/richardimaoka/go-sandbox
 
 go 1.22.3
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
-// リスト6.1
+// リスト6.4
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"io/ioutil"
 )
 
 type Post struct {
@@ -11,36 +14,38 @@ type Post struct {
 	Author  string
 }
 
-var PostById map[int]*Post
-var PostsByAuthor map[string][]*Post
+// store data
+func store(data interface{}, filename string) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+	if err != nil {
+		panic(err)
+	}
+}
 
-func store(post Post) {
-	PostById[post.Id] = &post
-	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
+// load the data
+func load(data interface{}, filename string) {
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	buffer := bytes.NewBuffer(raw)
+	dec := gob.NewDecoder(buffer)
+	err = dec.Decode(data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-
-	PostById = make(map[int]*Post)
-	PostsByAuthor = make(map[string][]*Post)
-
-	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
-	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
-	post3 := Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"}
-	post4 := Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"}
-
-	store(post1)
-	store(post2)
-	store(post3)
-	store(post4)
-
-	fmt.Println(PostById[1])
-	fmt.Println(PostById[2])
-
-	for _, post := range PostsByAuthor["Sau Sheong"] {
-		fmt.Println(post)
-	}
-	for _, post := range PostsByAuthor["Pedro"] {
-		fmt.Println(post)
-	}
+	post := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
+	store(post, "post1")
+	var postRead Post
+	load(&postRead, "post1")
+	fmt.Println(postRead)
 }
